a7b24f770c2bf37d08d9e68151484a4a87563b1b
Kai Schulz <info@sparrowmedia.de>
2025-03-11 09:12:41 +0100

    dl course material

--- public/wp-content/themes/sparrow/footer.php
+++
@@ -1,4 +0,0 @@
-<h2>Grüße vom Fußer</h2>
-<?php wp_footer(); ?>
-</body>
-</html>
--- public/wp-content/themes/sparrow/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <?php wp_head() ?>
-</head>
-
-<body>
-    <h1>Hallo vom Kopfer</h1>
