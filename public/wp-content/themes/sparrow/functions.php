baf263ceb180a1998d9935521af10c503f33680b
Kai Schulz <info@sparrowmedia.de>
2025-03-11 09:51:51 +0100

    loading style, script and media files

--- public/wp-content/themes/sparrow/functions.php
+++ public/wp-content/themes/sparrow/functions.php
@@ -1,8 +1,11 @@
 <?php
-function fictional_files() {
+function get_files() {
     wp_enqueue_style('fictional_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('fictional_extra_styles', get_theme_file_uri('/build/index.css'));
+    wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_script('main-javascript', get_theme_file_uri('/index.js'), array('jquery'), '1.0', true);
 };
 
-add_action('wp_enqueue_scripts', 'fictional_files');
+add_action('wp_enqueue_scripts', 'get_files');
 ?>
