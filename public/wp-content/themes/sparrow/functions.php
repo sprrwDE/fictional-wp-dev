5f91f81678d820533acd779329e02e6edd94ba08
Kai Schulz <info@sparrowmedia.de>
2025-03-11 09:25:54 +0100

    setting up static page to work with

--- public/wp-content/themes/sparrow/functions.php
+++ public/wp-content/themes/sparrow/functions.php
@@ -1,6 +1,7 @@
 <?php
 function fictional_files() {
-    wp_enqueue_style('fictional_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('fictional_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('fictional_extra_styles', get_theme_file_uri('/build/index.css'));
 };
 
 add_action('wp_enqueue_scripts', 'fictional_files');
